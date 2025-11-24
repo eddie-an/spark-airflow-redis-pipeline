96c1d5cc4a968936768d4505a316e7aa0e803ffc
Edward <edward.an@ucalgary.ca>
2025-11-23 17:46:13 -0700

    changes to incremental aggregation

--- processing/incremental/incremental_aggregation.py
+++ processing/incremental/incremental_aggregation.py
@@ -4,10 +4,9 @@ from pyspark.sql.types import *
 import os
 import redis
 from datetime import datetime
-from zoneinfo import ZoneInfo  
 
 def main():
-    execution_timestamp = datetime.now(ZoneInfo("Canada/Mountain"))
+    execution_timestamp = datetime.now()
     r = redis.Redis(host='redis', port=6379, db=0)
     exists = r.exists("processed_day")
 
@@ -22,33 +21,32 @@ def main():
 
     # Directories
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    output_path = os.path.join(BASE_DIR, f"../../data/processed/{start_day}")
-    csv_path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{start_day}/orders_{start_day}.csv')
+    output_csv_path = os.path.join(BASE_DIR, f"../../data/processed/{start_day}")
+    output_log_path = os.path.join(BASE_DIR, f"../../data/processed")
+    input_csv_path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{start_day}/orders_{start_day}.csv')
 
-    # Make sure the output directory exists (Spark won't overwrite unless you tell it)
-    os.makedirs(output_path, exist_ok=True) 
 
     # Get the first DataFrame
     df_combined = None
-    if os.path.exists(csv_path):
-        df_combined = spark.read.csv(csv_path, header=True, sep=",")
+    if os.path.exists(input_csv_path):
+        df_combined = spark.read.csv(input_csv_path, header=True, sep=",")
 
     # Get the next six DataFrames and combine them
     last_day = start_day
     for i in range(start_day+1, 7):
-        csv_path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{i}/orders_{i}.csv')
+        input_csv_path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{i}/orders_{i}.csv')
 
-        if not os.path.exists(csv_path): # If file doesn't exist, skip over it
-            print(f"Skipping {csv_path} — file doesn't exist.")
+        if not os.path.exists(input_csv_path): # If file doesn't exist, skip over it
+            print(f"Skipping {input_csv_path} — file doesn't exist.")
             continue
         
         last_day = i  # Update last day to be the last csv file (this is added to Redis)
-        df_new = spark.read.csv(csv_path, header=True, sep=",")
+        df_new = spark.read.csv(input_csv_path, header=True, sep=",")
         df_combined = df_combined.union(df_new)
 
     # Nothing to aggregate
     if df_combined is None:
-        with open(f'{output_path}/log.txt', "a") as logFile:
+        with open(f'{output_log_path}/log.txt', "a") as logFile:
             logFile.write(f'[{execution_timestamp}] Nothing has been aggregated.\n')
         return
     
@@ -63,12 +61,14 @@ def main():
 
     try:
         df_final.groupBy("order_dow", "order_hour_of_day", "category").count().show(50, truncate=False) # Show the final DataFrame
-        df_final.coalesce(1).write.mode("append").csv(output_path, header=True) # Append to csv
+            # Make sure the output directory exists (Spark won't overwrite unless you tell it)
+        os.makedirs(output_csv_path, exist_ok=True) 
+        df_final.coalesce(1).write.mode("append").csv(output_csv_path, header=True) # Append to csv
         r.set("processed_day", last_day)  # runs ONLY if write succeeds
 
-        with open(f'{output_path}/log.txt', "a") as logFile:
+        with open(f'{output_log_path}/log.txt', "a") as logFile:
             logFile.write(f"[{execution_timestamp}] Incremental Aggregation ran for days {start_day}-{last_day}. "
-            f"Output CSV written to {output_path}. Redis 'processed_day' updated to {last_day}.\n")
+            f"Output CSV written to {output_csv_path}. Redis 'processed_day' updated to {last_day}.\n")
     except Exception as e:
         print("Write failed, not updating Redis:", e)
 
