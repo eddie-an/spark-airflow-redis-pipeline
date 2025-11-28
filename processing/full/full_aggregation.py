1420004b34d39c100fe1a5165aa8a4544dabde72
Edward <edward.an@ucalgary.ca>
2025-11-27 21:10:57 -0700

    modularized incremental aggregation and made read.csv work regardless of file name

--- processing/full/full_aggregation.py
+++ processing/full/full_aggregation.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 import os
+import glob
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -16,23 +17,22 @@ spark = SparkSession.builder.master("local[*]").getOrCreate()
 #         .getOrCreate()
 # )
 
-def loadCSV():
-    csv_path = os.path.join(BASE_DIR, f'../../data/raw/0/orders_0.csv')
+def loadCSV(path):
     
     # Get the first DataFrame
     df_combined = None
-    if os.path.exists(csv_path):
-        df_combined = spark.read.csv(csv_path, header=True, sep=",")
+    if glob.glob(path):
+        df_combined = spark.read.csv(path, header=True, sep=",")
 
     # Get the next six DataFrames and combine them
     for i in range(1, 7):
-        csv_path = os.path.join(BASE_DIR, f'../../data/raw/{i}/orders_{i}.csv')
+        path = os.path.join(BASE_DIR, f'../../data/raw/{i}/*.csv')
         
-        if not os.path.exists(csv_path): # If file doesn't exist, skip over it
-            print(f"Skipping {csv_path} — file doesn't exist.")
+        if not glob.glob(path): # If file doesn't exist, skip over it
+            print(f"Skipping {path} — file doesn't exist.")
             continue
         
-        df_new = spark.read.csv(csv_path, header=True, sep=",")
+        df_new = spark.read.csv(path, header=True, sep=",")
         df_combined = df_combined.union(df_new)
     return df_combined
 
@@ -47,29 +47,25 @@ def processCSV(df):
     ).filter("items > 0")
     return df_final
 
-def saveCSV(df):
+def saveCSV(df, path):
     # Make sure the output directory exists (Spark won't overwrite unless you tell it)
-    output_path = os.path.join(BASE_DIR, "../../data/processed")
-    os.makedirs(output_path, exist_ok=True)
+    os.makedirs(path, exist_ok=True)
 
     # Save each DataFrame as a CSV
-    df.coalesce(1).write.mode("overwrite").csv(f"{output_path}", header=True)
+    df.coalesce(1).write.mode("overwrite").csv(f"{path}", header=True)
 
 def main():
-    df = loadCSV() # Load CSV files
+    csv_path = os.path.join(BASE_DIR, f'../../data/raw/0/*.csv')
+    output_path = os.path.join(BASE_DIR, "../../data/processed")
 
+    df = loadCSV(csv_path) # Load CSV files
     # If there is nothing to aggregate
     if df is None:
         print(f"The files don't exist")
         return
 
     df_processed = processCSV(df) # Process CSV so that the columns are changed
-
-    # Show the final DataFrame
-    df_processed.groupBy("order_dow", "order_hour_of_day", "category").count().show(50, truncate=False)
-
-
-    saveCSV(df_processed)
+    saveCSV(df_processed, output_path)
 
 
 if __name__== "__main__":
--- processing/incremental/incremental_aggregation.py
+++ processing/incremental/incremental_aggregation.py
@@ -2,76 +2,88 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 import os
+import glob
 import redis
 from datetime import datetime
 
+spark = SparkSession.builder.master("local[*]").getOrCreate() # Connect to Spark
+r = redis.Redis(host='redis', port=6379, db=0) # Connect to Redis
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def main():
     execution_timestamp = datetime.now()
-    r = redis.Redis(host='redis', port=6379, db=0)
-    exists = r.exists("processed_day")
-
     start_day = None
-    if exists == 0:
+    
+    if r.exists("processed_day") == 0: # If key doesn't exist in Redis, start from day 0
         start_day = 0
     else:
-        value = r.get('processed_day')
-        start_day = (int)(value.decode('utf-8'))+1
-
-    spark = SparkSession.builder.master("local[*]").getOrCreate()
-
+        value = r.get('processed_day') # Get last processed day from Redis
+        start_day = (int)(value.decode('utf-8'))+1 # Start from next day
+    
     # Directories
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    output_csv_path = os.path.join(BASE_DIR, f"../../data/processed/{start_day}")
     output_log_path = os.path.join(BASE_DIR, f"../../data/processed")
-    input_csv_path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{start_day}/orders_{start_day}.csv')
+    input_csv_path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{start_day}/*.csv')
+    output_csv_path = os.path.join(BASE_DIR, f"../../data/processed/{start_day}")
 
+    (df_combined, last_day) = loadCSV(input_csv_path, start_day)
 
-    # Get the first DataFrame
-    df_combined = None
-    if os.path.exists(input_csv_path):
-        df_combined = spark.read.csv(input_csv_path, header=True, sep=",")
-
-    # Get the next six DataFrames and combine them
-    last_day = start_day
-    for i in range(start_day+1, 7):
-        input_csv_path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{i}/orders_{i}.csv')
-
-        if not os.path.exists(input_csv_path): # If file doesn't exist, skip over it
-            print(f"Skipping {input_csv_path} — file doesn't exist.")
-            continue
-        
-        last_day = i  # Update last day to be the last csv file (this is added to Redis)
-        df_new = spark.read.csv(input_csv_path, header=True, sep=",")
-        df_combined = df_combined.union(df_new)
-
-    # Nothing to aggregate
+    # If nothing to aggregate
     if df_combined is None:
         with open(f'{output_log_path}/log.txt', "a") as logFile:
             logFile.write(f'[{execution_timestamp}] Nothing has been aggregated.\n')
         return
     
-    category_columns = df_combined.columns[4:]
-    stack_expr = ", ".join([f"'{c}', `{c}`" for c in category_columns])
-
-    df_final = df_combined.select(
-        "order_dow",
-        "order_hour_of_day",
-        expr(f"stack({len(category_columns)}, {stack_expr}) as (category, items)")
-    ).filter("items > 0")
+    df_final = processCSV(df_combined) # Process CSV so that the columns are changed
 
     try:
-        df_final.groupBy("order_dow", "order_hour_of_day", "category").count().show(50, truncate=False) # Show the final DataFrame
-            # Make sure the output directory exists (Spark won't overwrite unless you tell it)
-        os.makedirs(output_csv_path, exist_ok=True) 
-        df_final.coalesce(1).write.mode("append").csv(output_csv_path, header=True) # Append to csv
+        saveCSV(df_final, output_csv_path)
         r.set("processed_day", last_day)  # runs ONLY if write succeeds
-
         with open(f'{output_log_path}/log.txt', "a") as logFile:
             logFile.write(f"[{execution_timestamp}] Incremental Aggregation ran for days {start_day}-{last_day}. "
             f"Output CSV written to {output_csv_path}. Redis 'processed_day' updated to {last_day}.\n")
     except Exception as e:
         print("Write failed, not updating Redis:", e)
 
+def loadCSV(path, start_day):
+    # Get the first DataFrame
+    df_combined = None
+    if glob.glob(path):
+        df_combined = spark.read.csv(path, header=True, sep=",")
 
-if __name__=="__main__":
+    end_day = start_day
+    # Get the next six DataFrames and combine them
+    for i in range(start_day+1, 7):
+        path = os.path.join(BASE_DIR, f'../../data/incremental/raw/{i}/*.csv')
+
+        if not glob.glob(path): # If file doesn't exist, skip over it
+            print(f"Skipping {path} — file doesn't exist.")
+            continue
+        
+        end_day = i  # Update last day to be the last csv file (this is added to Redis)
+        df_new = spark.read.csv(path, header=True, sep=",")
+        df_combined = df_combined.union(df_new)
+
+    return (df_combined, end_day)
+
+def processCSV(df):
+    category_columns = df.columns[4:]
+    stack_expr = ", ".join([f"'{c}', `{c}`" for c in category_columns])
+
+    df_final = df.select(
+        "order_dow",
+        "order_hour_of_day",
+        expr(f"stack({len(category_columns)}, {stack_expr}) as (category, items)")
+    ).filter("items > 0")
+    return df_final
+
+def saveCSV(df, path):
+    # Make sure the output directory exists (Spark won't overwrite unless you tell it)
+    os.makedirs(path, exist_ok=True)
+
+    # Save each DataFrame as a CSV
+    df.coalesce(1).write.mode("overwrite").csv(f"{path}", header=True)
+
+
+if __name__== "__main__":
     main()
